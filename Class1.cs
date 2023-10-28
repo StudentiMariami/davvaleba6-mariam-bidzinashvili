d8b995e0dbe75381fd1a8cb0031681c91d2dd626
Mariami <Mariami@MARIAMI_ASUS>
2023-10-28 15:25:04 +0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davvaleba6
+{
+/*    
+ შექმენით კლასი, რომელშიც პრივატული მთელი ტიპის ცვლადია გამოცხადებული.
+მასთან მიმართვისათვის გამოიყენეთ თვისება, რომელშიც set და get მეთოდებია
+განსაზღვრული.თვისება პრივატულ ცვლადს დადებით მნიშვნელობებს ანიჭებს.*/
+    internal class Class1
+    {
+        private int cvladi=8;
+        public int tviseba
+        {
+            set
+            {
+                if(value > 0)  cvladi = value; 
+            }
+            get
+            {
+                return cvladi;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davvaleba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 obj_1= new Class1();
+            int cvladi=int.Parse(textBox1.Text);
+            label1.Text = obj_1.tviseba.ToString();
+            obj_1.tviseba = cvladi;
+            label2.Text=obj_1.tviseba.ToString();
+        }
+    }
+}
